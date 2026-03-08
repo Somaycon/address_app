b0f7da199027b1d4b929a89b376a9428ca199866
Somaycon <contato.somaycon@gmail.com>
2026-03-08 11:47:51 -0300

    feat: Apply Either

--- lib/features/home/domain/repositories/address_repository.dart
+++ lib/features/home/domain/repositories/address_repository.dart
@@ -1,5 +1,7 @@
+import 'package:address_app/core/error/failure.dart';
 import 'package:address_app/features/home/domain/entities/address_entity.dart';
+import 'package:fpdart/fpdart.dart';
 
 abstract class AddressRepository {
-  AddressEntity getAddress(String cep);
+  Future<Either<Failure, AddressEntity>> getAddress(String cep);
 }
