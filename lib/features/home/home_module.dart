f7097fa9ae88acb0365adbe829233cf162d53e61
Somaycon <contato.somaycon@gmail.com>
2026-03-08 12:07:22 -0300

    feat: Add GetAddressUsecase binding in HomeModule

--- lib/features/home/home_module.dart
+++ lib/features/home/home_module.dart
@@ -1,10 +1,18 @@
 import 'package:address_app/features/app_module.dart';
+import 'package:address_app/features/home/domain/usecase/get_address_usecase.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
   @override
+  void binds(Injector i) {
+    i.addLazySingleton(
+      () => GetAddressUsecase.new,
+    );
+    super.binds(i);
+  }
+
+  @override
   List<Module> get imports => [
     AppModule(),
   ];
-  
 }
