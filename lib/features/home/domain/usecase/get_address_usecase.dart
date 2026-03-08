45359704150808056fa9479719f2fe2887bf5e5e
Somaycon <contato.somaycon@gmail.com>
2026-03-08 11:48:23 -0300

    feat: Remove logic and adapt to the new repository rule

--- lib/features/home/domain/usecase/get_address_usecase.dart
+++ lib/features/home/domain/usecase/get_address_usecase.dart
@@ -1,21 +1,14 @@
 import 'package:address_app/core/error/failure.dart';
 import 'package:address_app/features/home/domain/entities/address_entity.dart';
 import 'package:address_app/features/home/domain/repositories/address_repository.dart';
+import 'package:fpdart/fpdart.dart';
 
 class GetAddressUsecase {
   final AddressRepository addressRepository;
 
   GetAddressUsecase({required this.addressRepository});
 
-  AddressEntity call(String cep) {
-    final cleanedCep = cep.replaceAll(RegExp(r'\D'), '');
-    if (cleanedCep.isEmpty) throw InvalidCepFailure('CEP não pode estar vazio');
-    if (cleanedCep.length != 8) {
-      throw InvalidCepFailure('O CEP deve conter exatamente 8 dígitos.');
-    }
-    if (!RegExp(r'^\d{8}$').hasMatch(cleanedCep)) {
-      throw InvalidCepFailure('CEP inválido.');
-    }
-    return addressRepository.getAddress(cleanedCep);
+  Future<Either<Failure, AddressEntity>> call(String cep) {
+    return addressRepository.getAddress(cep);
   }
 }
