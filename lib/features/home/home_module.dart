65a6f192915d751f2ffc20337734f8a679868300
Somaycon <contato.somaycon@gmail.com>
2026-03-08 13:26:17 -0300

    feat: Refactor HomeModule to include AddressDatasource and AddressRepository

--- lib/features/home/home_module.dart
+++ lib/features/home/home_module.dart
@@ -1,18 +1,23 @@
 import 'package:address_app/features/app_module.dart';
+import 'package:address_app/features/home/data/datasources/address_datasource.dart';
+import 'package:address_app/features/home/data/repositories/address_repository_impl.dart';
+import 'package:address_app/features/home/domain/repositories/address_repository.dart';
 import 'package:address_app/features/home/domain/usecase/get_address_usecase.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
   @override
+  List<Module> get imports => [
+    AppModule(),
+  ];
+
+  @override
   void binds(Injector i) {
     i.addLazySingleton(
       () => GetAddressUsecase.new,
     );
-    super.binds(i); 
+    i.addLazySingleton(AddressDatasource.new);
+    i.addLazySingleton<AddressRepository>(AddressRepositoryImpl.new);
+    super.binds(i);
   }
-
-  @override
-  List<Module> get imports => [
-    AppModule(),
-  ];
 }
