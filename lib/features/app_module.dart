c41547f98d88ddfb6831f360e904dedd50a130bb
Somaycon <contato.somaycon@gmail.com>
2026-03-08 11:32:59 -0300

    feat: Register Home module to app module

--- lib/features/app_module.dart
+++ lib/features/app_module.dart
@@ -1,3 +1,10 @@
+import 'package:address_app/features/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class AppModule extends Module {}
+class AppModule extends Module {
+  @override
+  void routes(RouteManager r) {
+    r.module('/', module: HomeModule());
+    super.routes(r);
+  }
+}
