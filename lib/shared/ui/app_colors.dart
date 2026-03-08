de50919168f26a2243f15432e2a9950a109728d1
Somaycon <contato.somaycon@gmail.com>
2026-03-08 11:07:19 -0300

    feat: add All the app colors

--- lib/shared/ui/app_colors.dart
+++ lib/shared/ui/app_colors.dart
@@ -1,5 +1,16 @@
 import 'dart:ui';
 
 class AppColors {
-  static const mainColor = Color(0xff2563EB);
+  static const darkBlue = Color(0xff155DFC);
+  static const lightBlue = Color(0xff51A2FF);
+  static const background = Color(0xffEFF6FF);
+  static const green = Color(0xff00C950);
+  static const lightYellow = Color(0xffFFB900);
+  static const yellow = Color(0xffFE9A00);
+  static const brown = Color(0xffBB4D00);
+  static const grey = Color(0xff6A7282);
+  static const lightGrey = Color(0xff99A1AF);
+  static const extraLightGrey = Color(0xffD1D5DC);
+  static const lightPink = Color(0xffFFA2A2);
+  static const red = Color(0xffFF6467);
 }
