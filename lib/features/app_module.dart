475833f615d11f85686f4ac6752a236a836d5ae9
Somaycon <contato.somaycon@gmail.com>
2026-03-09 19:37:09 -0300

    feat: Bind ApiClient

--- lib/features/app_module.dart
+++ lib/features/app_module.dart
@@ -1,7 +1,14 @@
+import 'package:address_app/core/network/api_client.dart';
 import 'package:address_app/features/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    i.addLazySingleton(ApiClient.new);
+    super.binds(i);
+  }
+
   @override
   void routes(RouteManager r) {
     r.module('/', module: HomeModule());
