ec43083d4b4764ab4b501ce8fd46642bd7e6fe95
Somaycon <contato.somaycon@gmail.com>
2026-03-20 13:17:00 -0300

    feat: add favorites module and shared preferences to AppModule

--- lib/features/app_module.dart
+++ lib/features/app_module.dart
@@ -1,17 +1,26 @@
 import 'package:address_app/core/network/api_client.dart';
+import 'package:address_app/features/favorites/favorites_module.dart';
 import 'package:address_app/features/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppModule extends Module {
+  final SharedPreferences sharedPreferences;
+
+  AppModule({
+    required this.sharedPreferences,
+  });
   @override
   void binds(Injector i) {
     i.addLazySingleton(ApiClient.new);
+    i.addInstance<SharedPreferences>(sharedPreferences);
     super.binds(i);
   }
 
   @override
   void routes(RouteManager r) {
     r.module('/', module: HomeModule());
+    r.module('/favorites', module: FavoritesModule());
     super.routes(r);
   }
 }
